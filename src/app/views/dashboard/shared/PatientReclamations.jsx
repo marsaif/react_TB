9048daca59644d134c47985d561641ef0659fbaf
= <=>
2022-05-06 00:09:21 +0100

    update url

--- src/app/views/dashboard/shared/PatientReclamations.jsx
+++ src/app/views/dashboard/shared/PatientReclamations.jsx
@@ -1,42 +1,42 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import axios from 'axios';
-import Grid from '@mui/material/Grid';
-import Moment from 'moment';
-import MarkEmailReadRoundedIcon from '@mui/icons-material/MarkEmailReadRounded';
-import UpdateIcon from '@mui/icons-material/Update';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import { TextField } from '@mui/material';
-import UpdateReclamationComponent from './UpdateReclamationComponent';
-import { ToastContainer, toast } from 'react-toastify';
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Collapse from '@mui/material/Collapse'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import { red } from '@mui/material/colors'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ShareIcon from '@mui/icons-material/Share'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import axios from 'axios'
+import Grid from '@mui/material/Grid'
+import Moment from 'moment'
+import MarkEmailReadRoundedIcon from '@mui/icons-material/MarkEmailReadRounded'
+import UpdateIcon from '@mui/icons-material/Update'
+import DeleteIcon from '@mui/icons-material/Delete'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Modal from '@mui/material/Modal'
+import { TextField } from '@mui/material'
+import UpdateReclamationComponent from './UpdateReclamationComponent'
+import { ToastContainer, toast } from 'react-toastify'
 
 const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
+    const { expand, ...other } = props
+    return <IconButton {...other} />
 })(({ theme, expand }) => ({
     transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
     marginLeft: 'auto',
     transition: theme.transitions.create('transform', {
         duration: theme.transitions.duration.shortest,
     }),
-}));
+}))
 
 const style = {
     position: 'absolute',
@@ -48,190 +48,206 @@ const style = {
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
-};
+}
 
 export default function PatientReclamations() {
+    const [expanded, setExpanded] = React.useState(false)
+    const [reclamations, setReclamations] = React.useState([])
+    const [patient, setPatient] = React.useState()
+    const [doctor, setDoctor] = React.useState()
+    const [selectedRow, setSelectedRow] = React.useState()
+    const [selectedId, setSelectedId] = React.useState()
+    const [input, setInput] = React.useState()
+    const [open, setOpen] = React.useState(false)
+    const [SearchField, setSearchField] = React.useState()
 
-    const [expanded, setExpanded] = React.useState(false);
-    const [reclamations, setReclamations] = React.useState([]);
-    const [patient, setPatient] = React.useState();
-    const [doctor, setDoctor] = React.useState();
-    const [selectedRow, setSelectedRow] = React.useState();
-    const [selectedId, setSelectedId] = React.useState();
-    const [input, setInput] = React.useState();
-    const [open, setOpen] = React.useState(false);
-    const [SearchField, setSearchField] = React.useState();
+    const [lst, setLst] = React.useState([])
 
-    const [lst, setLst] = React.useState([]);
-
-
-    const handleClose = () => setOpen(false);
-
+    const handleClose = () => setOpen(false)
 
     React.useEffect(() => {
         console.log(open)
         fetchreclamation()
-    }, []);
+    }, [])
 
     React.useEffect(() => {
         const accessToken = localStorage.getItem('accessToken')
         axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`
-        axios.get("http://localhost:3001/users/getUser").then((response) => {
-            setPatient(response.data.user)
-            console.log(response)
-        })
-    }, []);
+        axios
+            .get('https://tbibi.herokuapp.com/users/getUser')
+            .then((response) => {
+                setPatient(response.data.user)
+                console.log(response)
+            })
+    }, [])
 
     const handleChangeSearch = (newValue) => {
-        
         setLst([])
         console.log(newValue.target.value)
         setSearchField(newValue.target.value)
-        if(newValue.target.value.length>0){
-            reclamations.forEach(element => {
-                if (element.doctor.firstName.includes(SearchField)){
+        if (newValue.target.value.length > 0) {
+            reclamations.forEach((element) => {
+                if (element.doctor.firstName.includes(SearchField)) {
                     console.log(element)
                     lst.push(element)
                     setReclamations(lst)
-    
                 }
-               
-    
-            });
-        }
-        else {
+            })
+        } else {
             fetchreclamation()
-
         }
-
     }
 
     const fetchreclamation = (data) => {
-        axios.get("http://localhost:3001/reclamations").then((res) => {
+        axios.get('https://tbibi.herokuapp.com/reclamations').then((res) => {
             //    lst = res.data
             setReclamations(res.data)
-
-
         })
     }
     const handleClick = (data) => {
         console.log(data)
-        axios.put("http://localhost:3001/reclamations/" + data._id).then((res) => {
-            fetchreclamation()
-
-        })
-    };
+        axios
+            .put('https://tbibi.herokuapp.com/reclamations/' + data._id)
+            .then((res) => {
+                fetchreclamation()
+            })
+    }
 
     const handleDeleteClick = (data) => {
         console.log(data)
-        axios.delete("http://localhost:3001/reclamations/" + data._id).then((res) => {
-            fetchreclamation()
+        axios
+            .delete('https://tbibi.herokuapp.com/reclamations/' + data._id)
+            .then((res) => {
+                fetchreclamation()
+            })
 
-        })
-
-        toast("Reclamation deleted successfully")
-    };
+        toast('Reclamation deleted successfully')
+    }
 
     const handleOpen = (row) => {
         console.log(row)
         setSelectedRow(row.description)
         setSelectedId(row._id)
-        setOpen(true);
+        setOpen(true)
     }
 
     const handleUpdateClick = () => {
-        console.log("clickedddddddd")
+        console.log('clickedddddddd')
         console.log(input)
-        axios.put("http://localhost:3001/reclamations/updateReclamationDescription/" + selectedId, {
-            description: input,
-        }).then((res) => {
-            fetchreclamation()
-            setOpen(false)
-        })
-    };
+        axios
+            .put(
+                'https://tbibi.herokuapp.com/reclamations/updateReclamationDescription/' +
+                    selectedId,
+                {
+                    description: input,
+                }
+            )
+            .then((res) => {
+                fetchreclamation()
+                setOpen(false)
+            })
+    }
     const pull_data = (data) => {
         handleUpdateClick()
-
     }
 
     return (
-       
-            <Grid container spacing={2}>
-
-                {reclamations.map((row) => (
-
-                    <Grid item xs={4} key={Math.random().toString(36).substr(2, 9)}>
-
-                        {(() => {
-                            if (row.read == false && row.patient._id === patient._id) {
-                                return (
-                                    <Card sx={{ maxWidth: 345 }} >
-                                        <CardHeader
-                                            avatar={
-                                                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                                                    R
-                                                </Avatar>
-                                            }
-                                            action={
-                                                <IconButton aria-label="settings" onClick={() => { handleClick(row); }}>
-
-                                                </IconButton>
-
-                                            }
-                                            title={row.patient.firstName}
-                                            subheader={Moment(row.date).format('d MMM YYYY')}
-                                        />
-
-                                        <CardContent>
-                                            <Typography variant="body2" color="text.secondary">
-
-                                                About Doctor : {row.doctor.firstName} <br />
-                                                Doctor's email : {row.doctor.email} <br />
-                                                Description : {row.description}
-                                            </Typography>
-                                        </CardContent>
-
-                                        <CardActions disableSpacing>
-                                            <IconButton aria-label="add to favorites" onClick={() => { handleOpen(row); }}>
-                                                <UpdateIcon />
-                                            </IconButton>
-                                            <IconButton aria-label="share" onClick={() => { handleDeleteClick(row); }}>
-                                                <DeleteIcon />
-                                            </IconButton>
-                                            <Modal
-                                                open={open}
-                                                onClose={handleClose}
-                                                aria-labelledby="modal-modal-title"
-                                                aria-describedby="modal-modal-description"
+        <Grid container spacing={2}>
+            {reclamations.map((row) => (
+                <Grid item xs={4} key={Math.random().toString(36).substr(2, 9)}>
+                    {(() => {
+                        if (
+                            row.read == false &&
+                            row.patient._id === patient._id
+                        ) {
+                            return (
+                                <Card sx={{ maxWidth: 345 }}>
+                                    <CardHeader
+                                        avatar={
+                                            <Avatar
+                                                sx={{ bgcolor: red[500] }}
+                                                aria-label="recipe"
                                             >
+                                                R
+                                            </Avatar>
+                                        }
+                                        action={
+                                            <IconButton
+                                                aria-label="settings"
+                                                onClick={() => {
+                                                    handleClick(row)
+                                                }}
+                                            ></IconButton>
+                                        }
+                                        title={row.patient.firstName}
+                                        subheader={Moment(row.date).format(
+                                            'd MMM YYYY'
+                                        )}
+                                    />
 
-                                                <UpdateReclamationComponent idrecla={selectedId} func={pull_data}  ></UpdateReclamationComponent>
+                                    <CardContent>
+                                        <Typography
+                                            variant="body2"
+                                            color="text.secondary"
+                                        >
+                                            About Doctor :{' '}
+                                            {row.doctor.firstName} <br />
+                                            Doctor's email : {
+                                                row.doctor.email
+                                            }{' '}
+                                            <br />
+                                            Description : {row.description}
+                                        </Typography>
+                                    </CardContent>
 
-                                            </Modal>
-                                        </CardActions>
-                                    </Card>
+                                    <CardActions disableSpacing>
+                                        <IconButton
+                                            aria-label="add to favorites"
+                                            onClick={() => {
+                                                handleOpen(row)
+                                            }}
+                                        >
+                                            <UpdateIcon />
+                                        </IconButton>
+                                        <IconButton
+                                            aria-label="share"
+                                            onClick={() => {
+                                                handleDeleteClick(row)
+                                            }}
+                                        >
+                                            <DeleteIcon />
+                                        </IconButton>
+                                        <Modal
+                                            open={open}
+                                            onClose={handleClose}
+                                            aria-labelledby="modal-modal-title"
+                                            aria-describedby="modal-modal-description"
+                                        >
+                                            <UpdateReclamationComponent
+                                                idrecla={selectedId}
+                                                func={pull_data}
+                                            ></UpdateReclamationComponent>
+                                        </Modal>
+                                    </CardActions>
+                                </Card>
+                            )
+                        }
 
-                                )
-                            }
-
-                            return null;
-                        })()}
-
-                    </Grid>
-                ))}
-                <ToastContainer
-                    position="top-center"
-                    autoClose={5000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                />
-            </Grid>
-      
-
-    );
+                        return null
+                    })()}
+                </Grid>
+            ))}
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </Grid>
+    )
 }
--- src/app/views/dashboard/shared/UpdateReclamationComponent.jsx
+++ src/app/views/dashboard/shared/UpdateReclamationComponent.jsx
@@ -1,33 +1,32 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import axios from 'axios';
-import Grid from '@mui/material/Grid';
-import Moment from 'moment';
-import MarkEmailReadRoundedIcon from '@mui/icons-material/MarkEmailReadRounded';
-import UpdateIcon from '@mui/icons-material/Update';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import { TextField } from '@mui/material';
-import PatientReclamations from './PatientReclamations';
-import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Collapse from '@mui/material/Collapse'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import { red } from '@mui/material/colors'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ShareIcon from '@mui/icons-material/Share'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import axios from 'axios'
+import Grid from '@mui/material/Grid'
+import Moment from 'moment'
+import MarkEmailReadRoundedIcon from '@mui/icons-material/MarkEmailReadRounded'
+import UpdateIcon from '@mui/icons-material/Update'
+import DeleteIcon from '@mui/icons-material/Delete'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Modal from '@mui/material/Modal'
+import { TextField } from '@mui/material'
+import PatientReclamations from './PatientReclamations'
+import { useNavigate } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify'
 
 const style = {
     position: 'absolute',
@@ -39,21 +38,20 @@ const style = {
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
-};
-
+}
 
 export default function UpdateReclamationComponent(props) {
     let lst = []
-    const navigate = useNavigate();
+    const navigate = useNavigate()
 
-    const [input, setInput] = React.useState();
-    const [open, setOpen] = React.useState(false);
-    const [reclamations, setReclamations] = React.useState([]);
+    const [input, setInput] = React.useState()
+    const [open, setOpen] = React.useState(false)
+    const [reclamations, setReclamations] = React.useState([])
 
-    const handleClose = () => setOpen(false);
+    const handleClose = () => setOpen(false)
 
     const fetchreclamation = (data) => {
-        axios.get("http://localhost:3001/reclamations").then((res) => {
+        axios.get('https://tbibi.herokuapp.com/reclamations').then((res) => {
             lst = res.data
             console.log(res.data)
             setReclamations(res.data)
@@ -61,37 +59,51 @@ export default function UpdateReclamationComponent(props) {
     }
 
     const handleUpdateClick = () => {
-        console.log("clickedddddddCHILD")
+        console.log('clickedddddddCHILD')
 
         console.log(props.idrecla)
 
-        axios.put("http://localhost:3001/reclamations/updateReclamationDescription/" + props.idrecla, {
-            description: input,
-
-        }).then((res) => {
-            props.func('My name is Dean Winchester & this is my brother Sammie');
-            fetchreclamation()
-            toast("Reclamation updated successfully")
-
-        })
-
-    };
+        axios
+            .put(
+                'https://tbibi.herokuapp.com/reclamations/updateReclamationDescription/' +
+                    props.idrecla,
+                {
+                    description: input,
+                }
+            )
+            .then((res) => {
+                props.func(
+                    'My name is Dean Winchester & this is my brother Sammie'
+                )
+                fetchreclamation()
+                toast('Reclamation updated successfully')
+            })
+    }
 
     return (
-
         <Box sx={style}>
             <Typography id="modal-modal-title" variant="h6" component="h4">
                 Update Details :
             </Typography>
-            <TextField style={{ width: '100%' }}
+            <TextField
+                style={{ width: '100%' }}
                 name="details"
                 id="outlined-multiline-static"
                 label="Details"
                 multiline
                 rows={4}
-                onInput={e => setInput(e.target.value)}
+                onInput={(e) => setInput(e.target.value)}
             />
-            <Button variant="contained" style={{ marginTop: '20px' }} onClick={() => { handleUpdateClick(); }} type="submit">Update</Button>
+            <Button
+                variant="contained"
+                style={{ marginTop: '20px' }}
+                onClick={() => {
+                    handleUpdateClick()
+                }}
+                type="submit"
+            >
+                Update
+            </Button>
             <ToastContainer
                 position="top-center"
                 autoClose={5000}
@@ -104,6 +116,5 @@ export default function UpdateReclamationComponent(props) {
                 pauseOnHover
             />
         </Box>
-
-    );
+    )
 }
